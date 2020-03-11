95724c2481ffd5785cefd9bfa01f050c1f112553
Alice <agenkin1@gmail.com>
2020-03-11 18:48:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,9 @@
 def "mario"
-  puts "puts out 'It's-a me, Mario!'"
+  puts "It's-a me, Mario!""
 end
 
 def "toadstool"
-  puts "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'"
+  puts "Thank You Mario! But Our Princess Is In Another Castle!"
 end
 
 def "link"
